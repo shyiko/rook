52a0b9a432c14ade4d934f085740ad85b166d5c7
Stanley Shyiko <stanley.shyiko@gmail.com>
2013-06-13 14:22:45 +0300

    Fixed root-target-hibernate-cache dependencies scope, file headers, access modifiers & some checkstyle violations

--- rook-target-hibernate-cache/src/test/java/com/github/shyiko/rook/target/hibernate/cache/SynchronizerContextTest.java
+++ rook-target-hibernate-cache/src/test/java/com/github/shyiko/rook/target/hibernate/cache/SynchronizerContextTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2013 Ivan Zaytsev
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.github.shyiko.rook.target.hibernate.cache;
 
 import com.github.shyiko.rook.target.hibernate.cache.model.DummyEntity;
@@ -16,7 +31,6 @@ import java.util.*;
 
 /**
  * @author <a href="mailto:ivan.zaytsev@webamg.com">Ivan Zaytsev</a>
- *         2013-06-12
  */
 @ContextConfiguration(locations = {
         "classpath:hibernate-spring-test-context.xml"
@@ -24,17 +38,15 @@ import java.util.*;
 public class SynchronizerContextTest extends AbstractTransactionalTestNGSpringContextTests {
 
     @Autowired
-    SessionFactory sessionFactory;
-
+    private SessionFactory sessionFactory;
     @Autowired
-    LocalSessionFactoryBean sessionFactoryBean;
-
+    private LocalSessionFactoryBean sessionFactoryBean;
 
     private String[] mapColumnValues(String entityName, Map<String, String> columnValuesByName) {
         Table table = sessionFactoryBean.getConfiguration().getClassMapping(entityName).getTable();
         List<String> result = new ArrayList<String>();
-
-        for (@SuppressWarnings("unchecked") Iterator<Column> iterator = table.getColumnIterator(); iterator.hasNext(); ) {
+        for (@SuppressWarnings("unchecked") Iterator<Column> iterator = table.getColumnIterator();
+             iterator.hasNext(); ) {
             Column column = iterator.next();
             if (columnValuesByName.containsKey(column.getName())) {
                 result.add(columnValuesByName.get(column.getName()));
@@ -45,42 +57,42 @@ public class SynchronizerContextTest extends AbstractTransactionalTestNGSpringCo
 
     @Test
     public void testKeyMappings() throws Exception {
-        SynchronizationContext synchronizationContext = new SynchronizationContext(sessionFactoryBean.getConfiguration(), sessionFactory);
-
+        SynchronizationContext synchronizationContext = new SynchronizationContext(
+                sessionFactoryBean.getConfiguration(), sessionFactory);
 
         {
-            List<EvictionTarget> dummyEvictionTargets = new ArrayList<EvictionTarget>(synchronizationContext.getEvictionTargets("rook.dummy_entity"));
-
-            String[] allFieldsFofDummy = mapColumnValues(DummyEntity.class.getName(), new LinkedHashMap<String, String>() {{
-                put("id", "id");
-                put("name", "name");
-            }});
-
-            String[] keyFieldsFofDummy = mapColumnValues(DummyEntity.class.getName(), new LinkedHashMap<String, String>() {{
-                put("id", "id");
-            }});
-
+            List<EvictionTarget> dummyEvictionTargets = new ArrayList<EvictionTarget>(synchronizationContext.
+                    getEvictionTargets("rook.dummy_entity"));
+            String[] allFieldsFofDummy = mapColumnValues(DummyEntity.class.getName(),
+                    new LinkedHashMap<String, String>() {{
+                        put("id", "id");
+                        put("name", "name");
+                    }});
+            String[] keyFieldsFofDummy = mapColumnValues(DummyEntity.class.getName(),
+                    new LinkedHashMap<String, String>() {{
+                        put("id", "id");
+                    }});
             Assert.assertEquals(dummyEvictionTargets.get(0).getPrimaryKey().of(allFieldsFofDummy), keyFieldsFofDummy);
         }
 
-
         {
-            List<EvictionTarget> dummyEvictionTargets = new ArrayList<EvictionTarget>(synchronizationContext.getEvictionTargets("rook.dummy_entity_2fpk"));
+            List<EvictionTarget> dummyEvictionTargets = new ArrayList<EvictionTarget>(synchronizationContext.
+                    getEvictionTargets("rook.dummy_entity_2fpk"));
+            String[] allFieldsFofDummy2 = mapColumnValues(DummyEntityTwoFieldPK.class.getName(),
+                    new LinkedHashMap<String, String>() {{
+                        put("id", "id");
+                        put("id2", "id2");
+                        put("name", "name");
+                    }});
 
-            String[] allFieldsFofDummy2 = mapColumnValues(DummyEntityTwoFieldPK.class.getName(), new LinkedHashMap<String, String>() {{
-                put("id", "id");
-                put("id2", "id2");
-                put("name", "name");
-            }});
-
-            String[] keyFieldsFofDummy2 = mapColumnValues(DummyEntityTwoFieldPK.class.getName(), new LinkedHashMap<String, String>() {{
-                put("id", "id");
-                put("id2", "id2");
-            }});
+            String[] keyFieldsFofDummy2 = mapColumnValues(DummyEntityTwoFieldPK.class.getName(),
+                    new LinkedHashMap<String, String>() {{
+                        put("id", "id");
+                        put("id2", "id2");
+                    }});
 
             Assert.assertEquals(dummyEvictionTargets.get(0).getPrimaryKey().of(allFieldsFofDummy2), keyFieldsFofDummy2);
         }
-
     }
 
 }
