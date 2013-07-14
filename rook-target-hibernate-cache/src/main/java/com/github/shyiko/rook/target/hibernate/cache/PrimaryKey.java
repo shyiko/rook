shyiko/rook

commit 8134ecf5b7058e03c56a0dc2f1857617e6e0ee09

fix #1 Support composite keys (Hibernate)

rook-target-hibernate-cache/src/main/java/com/github/shyiko/rook/target/hibernate/cache/PrimaryKey.java
@@ -15,24 +15,99 @@
  */
 package com.github.shyiko.rook.target.hibernate.cache;
 
+import org.hibernate.mapping.Collection;
+import org.hibernate.mapping.Column;
+import org.hibernate.mapping.KeyValue;
+import org.hibernate.mapping.PersistentClass;
+import org.hibernate.mapping.Selectable;
+import org.hibernate.mapping.Table;
+
 import java.io.Serializable;
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 /**
  * @author <a href="mailto:stanley.shyiko@gmail.com">Stanley Shyiko</a>
  */
 public class PrimaryKey {
 
-    private final int[] positionWithinRow;
+    private Class entityClass = null;
+    private final KeyColumn[] positionWithinRow;
 
-    public PrimaryKey(int[] positionWithinRow) {
-        this.positionWithinRow = positionWithinRow;
+    private Map<String, Integer> asIndexMap(Table table) {
+        Map<String, Integer> columnIndexByName = new HashMap<String, Integer>();
+        int index = 0;
+        for (@SuppressWarnings("unchecked") Iterator<Column> columnIterator = table.getColumnIterator();
+             columnIterator.hasNext(); ) {
+            Column column = columnIterator.next();
+            columnIndexByName.put(column.getName(), index++);
+        }
+        return columnIndexByName;
     }
 
-    public Serializable[] of(Serializable[] row) {
-        Serializable[] result = new Serializable[positionWithinRow.length];
-        for (int index = positionWithinRow.length - 1; index > -1; index--) {
-            result[index] = row[positionWithinRow[index]];
+    public PrimaryKey(Collection collection) {
+        this(collection.getKey(), collection.getTable());
+    }
+
+    public PrimaryKey(PersistentClass persistentClass) {
+        this(persistentClass.getKey(), persistentClass.getTable());
+        entityClass = persistentClass.getMappedClass();
+    }
+
+    private PrimaryKey(KeyValue keyValue, Table table) {
+        Map<String, Integer> indexMap = asIndexMap(table);
+        KeyColumn[] pkIndexes = new KeyColumn[keyValue.getColumnSpan()];
+        int index = 0;
+        for (@SuppressWarnings("unchecked") Iterator<Selectable> columnIterator = keyValue.getColumnIterator();
+             columnIterator.hasNext(); ) {
+            Column column = (Column) columnIterator.next(); // todo: what if it's Formula?
+            pkIndexes[index++] =
+                    new KeyColumn(column.getName(), indexMap.get(column.getName()));
         }
-        return result;
+        this.positionWithinRow = pkIndexes;
+    }
+
+    /**
+     * Get identifier for given row values
+     *
+     * @param row values
+     * @return Serializable key
+     */
+    public Serializable getIdentifier(Serializable[] row) {
+        if (positionWithinRow.length == 1) {
+            return row[positionWithinRow[0].index];
+        } else if (positionWithinRow.length > 1) {
+            if (entityClass == null) {
+                throw new IllegalStateException("Entity class is undefined! Cannot create key.");
+            }
+            try {
+                Serializable key = (Serializable) entityClass.newInstance();
+                for (KeyColumn keyColumn : positionWithinRow) {
+                    Field field = entityClass.getDeclaredField(keyColumn.name);
+                    field.setAccessible(true);
+                    field.set(key, row[keyColumn.index]);
+                }
+                return key;
+            } catch (Throwable e) {
+                throw new IllegalStateException("Cannot instantiate entity key", e);
+            }
+        } else {
+            throw new IllegalStateException("There are zero key columns in given key");
+        }
+    }
+
+    /**
+     * Class that contains single key column data
+     */
+    private static final class KeyColumn {
+        private final String name;
+        private final int index;
+
+        private KeyColumn(String name, int index) {
+            this.name = name;
+            this.index = index;
+        }
     }
 }

rook-target-hibernate-cache/src/main/java/com/github/shyiko/rook/target/hibernate/cache/SecondLevelCacheSynchronizer.java
@@ -60,11 +60,7 @@ public class SecondLevelCacheSynchronizer implements ReplicationListener {
         String qualifiedName = event.getSchema().toLowerCase() + "." + event.getTable().toLowerCase();
         Collection<EvictionTarget> evictionTargets = synchronizationContext.getEvictionTargets(qualifiedName);
         for (EvictionTarget evictionTarget : evictionTargets) {
-            Serializable[] id = evictionTarget.getPrimaryKey().of(event.getValues());
-            if (id.length != 1) {
-                throw new UnsupportedOperationException(); // todo(shyiko): yet to implement
-            }
-            Serializable key = id[0];
+            Serializable key = evictionTarget.getPrimaryKey().getIdentifier(event.getValues());
             if (logger.isDebugEnabled()) {
                 logger.debug("Evicting " + evictionTarget.getName() + "#" + key);
             }

rook-target-hibernate-cache/src/main/java/com/github/shyiko/rook/target/hibernate/cache/SynchronizationContext.java
@@ -18,10 +18,7 @@ package com.github.shyiko.rook.target.hibernate.cache;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.engine.spi.SessionFactoryImplementor;
-import org.hibernate.mapping.Column;
-import org.hibernate.mapping.KeyValue;
 import org.hibernate.mapping.PersistentClass;
-import org.hibernate.mapping.Selectable;
 import org.hibernate.mapping.Table;
 
 import java.util.Collection;
@@ -63,8 +60,7 @@ public class SynchronizationContext {
             PersistentClass persistentClass = iterator.next();
             Table table = persistentClass.getTable();
             String className = persistentClass.getClassName();
-            Map<String, Integer> columnIndexByName = asIndexMap(table);
-            PrimaryKey primaryKey = asPrimaryKey(persistentClass.getKey(), columnIndexByName);
+            PrimaryKey primaryKey = new PrimaryKey(persistentClass);
             evictionTargetsOf(table).add(new EvictionTarget(className, primaryKey, false));
         }
     }
@@ -76,8 +72,7 @@ public class SynchronizationContext {
             org.hibernate.mapping.Collection collection = iterator.next();
             Table table = collection.getCollectionTable();
             String role = collection.getRole();
-            Map<String, Integer> columnIndexByName = asIndexMap(table);
-            PrimaryKey primaryKey = asPrimaryKey(collection.getKey(), columnIndexByName);
+            PrimaryKey primaryKey = new PrimaryKey(collection);
             evictionTargetsOf(table).add(new EvictionTarget(role, primaryKey, true));
         }
     }
@@ -90,28 +85,4 @@ public class SynchronizationContext {
         }
         return evictionTargets;
     }
-
-    private Map<String, Integer> asIndexMap(Table table) {
-        Map<String, Integer> columnIndexByName = new HashMap<String, Integer>();
-        int index = 0;
-        for (@SuppressWarnings("unchecked") Iterator<Column> columnIterator = table.getColumnIterator();
-             columnIterator.hasNext(); ) {
-            Column column = columnIterator.next();
-            columnIndexByName.put(column.getName(), index++);
-        }
-        return columnIndexByName;
-    }
-
-    @SuppressWarnings("unchecked")
-    private PrimaryKey asPrimaryKey(KeyValue keyValue, Map<String, Integer> indexMap) {
-        int[] pkIndexes = new int[keyValue.getColumnSpan()];
-        int index = 0;
-        for (Iterator<Selectable> columnIterator = keyValue.getColumnIterator();
-             columnIterator.hasNext(); ) {
-            Column column = (Column) columnIterator.next(); // todo: what if it's Formula?
-            pkIndexes[index++] = indexMap.get(column.getName());
-        }
-        return new PrimaryKey(pkIndexes);
-    }
-
 }

rook-target-hibernate-cache/src/test/java/com/github/shyiko/rook/target/hibernate/cache/HibernateIdentifierTest.java
@@ -1,0 +1,70 @@
+/*
+ * Copyright 2013 Ivan Zaytsev
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.shyiko.rook.target.hibernate.cache;
+
+import com.github.shyiko.rook.target.hibernate.cache.model.DummyEntity;
+import com.github.shyiko.rook.target.hibernate.cache.model.DummyEntityTwoFieldPK;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTransactionalTestNGSpringContextTests;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.Serializable;
+
+/**
+ * @author <a href="mailto:ivan.zaytsev@webamg.com">Ivan Zaytsev</a>
+ */
+@ContextConfiguration(locations = {
+        "classpath:hibernate-spring-test-context.xml"
+})
+public class HibernateIdentifierTest extends AbstractTransactionalTestNGSpringContextTests {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Test
+    public void testCacheSave() throws Exception {
+        Session session = sessionFactory.getCurrentSession();
+
+        DummyEntityTwoFieldPK entity = new DummyEntityTwoFieldPK();
+        entity.setId(1L);
+        entity.setId2(2L);
+        entity.setName("name");
+        session.save(entity);
+        DummyEntityTwoFieldPK dummyKey = new DummyEntityTwoFieldPK();
+        dummyKey.setId(1L);
+        dummyKey.setId2(2L);
+        Serializable identifier = session.getIdentifier(entity);
+        Assert.assertEquals(identifier.getClass(), DummyEntityTwoFieldPK.class);
+        Object o = session.get(DummyEntityTwoFieldPK.class, dummyKey);
+        Assert.assertNotNull(o);
+    }
+
+    @Test
+    public void testOnePK() throws Exception {
+        Session session = sessionFactory.getCurrentSession();
+
+        DummyEntity entity = new DummyEntity();
+        entity.setId(1L);
+        entity.setName("name");
+        session.save(entity);
+        Serializable identifier = session.getIdentifier(entity);
+        Assert.assertEquals(identifier.getClass(), Long.class);
+    }
+}

rook-target-hibernate-cache/src/test/java/com/github/shyiko/rook/target/hibernate/cache/SecondLevelSynchronizerEvictionTest.java
@@ -1,0 +1,127 @@
+/*
+ * Copyright 2013 Ivan Zaytsev
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.shyiko.rook.target.hibernate.cache;
+
+import com.github.shyiko.rook.api.event.DeleteRowReplicationEvent;
+import com.github.shyiko.rook.target.hibernate.cache.model.DummyEntityTwoFieldPK;
+import org.hibernate.Cache;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate4.HibernateTransactionManager;
+import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.Serializable;
+import java.util.HashMap;
+
+import static org.testng.Assert.assertFalse;
+
+/**
+ * @author <a href="mailto:ivan.zaytsev@webamg.com">Ivan Zaytsev</a>
+ *         2013-06-27
+ */
+@ContextConfiguration(locations = {
+        "classpath:hibernate-ehcache-spring-test-context.xml"
+})
+public class SecondLevelSynchronizerEvictionTest extends AbstractTestNGSpringContextTests {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+    @Autowired
+    private LocalSessionFactoryBean sessionFactoryBean;
+
+    @Autowired
+    private HibernateTransactionManager transactionManager;
+    private SecondLevelCacheSynchronizer secondLevelCacheSynchronizer;
+    private Serializable[] values;
+    private Cache cache;
+    private Serializable identifier;
+
+    private void init() {
+        SynchronizationContext synchronizationContext = new SynchronizationContext(
+                sessionFactoryBean.getConfiguration(), sessionFactory);
+
+        secondLevelCacheSynchronizer = new SecondLevelCacheSynchronizer(synchronizationContext);
+
+        values = TestHelper.mapColumnValues(sessionFactoryBean.getConfiguration(),
+                DummyEntityTwoFieldPK.class.getName(), new HashMap<String, Serializable>() {
+            {
+                put("id", 1L);
+                put("id2", 2L);
+            }
+        });
+
+        cache = synchronizationContext.getSessionFactory().getCache();
+
+        identifier = new Serializable[1];
+    }
+
+    @Test
+    public void testKeyMappingsForDummyTwoPk() throws Exception {
+        init();
+
+        TransactionTemplate transactionTemplate = new TransactionTemplate(transactionManager);
+        transactionTemplate.execute(new TransactionCallbackWithoutResult() {
+            @Override
+            protected void doInTransactionWithoutResult(TransactionStatus status) {
+                DummyEntityTwoFieldPK entity = new DummyEntityTwoFieldPK();
+                entity.setId(1L);
+                entity.setId2(2L);
+                entity.setName("name");
+
+                final Session session = sessionFactory.getCurrentSession();
+                session.save(entity);
+
+                identifier = session.getIdentifier(entity);
+
+                session.flush();
+                session.clear();
+            }
+        });
+
+        new TransactionTemplate(transactionManager).execute(new TransactionCallbackWithoutResult() {
+            @Override
+            protected void doInTransactionWithoutResult(TransactionStatus status) {
+                final Session session = sessionFactory.getCurrentSession();
+                Object entity1 = session.get(DummyEntityTwoFieldPK.class, identifier);
+                Assert.assertNotNull(entity1);
+            }
+        });
+
+        Assert.assertTrue(cache.containsEntity(DummyEntityTwoFieldPK.class, identifier));
+
+        new TransactionTemplate(transactionManager).execute(new TransactionCallbackWithoutResult() {
+            @Override
+            protected void doInTransactionWithoutResult(TransactionStatus status) {
+                final Session session = sessionFactory.getCurrentSession();
+                Object entity1 = session.get(DummyEntityTwoFieldPK.class, identifier);
+                Assert.assertNotNull(entity1);
+            }
+        });
+
+        secondLevelCacheSynchronizer.onEvent(new DeleteRowReplicationEvent("rook", "dummy_entity_2fpk", values));
+
+        assertFalse(cache.containsEntity(DummyEntityTwoFieldPK.class, identifier));
+    }
+
+}

rook-target-hibernate-cache/src/test/java/com/github/shyiko/rook/target/hibernate/cache/SynchronizerContextTest.java
@@ -17,6 +17,7 @@ package com.github.shyiko.rook.target.hibernate.cache;
 
 import com.github.shyiko.rook.target.hibernate.cache.model.DummyEntity;
 import com.github.shyiko.rook.target.hibernate.cache.model.DummyEntityTwoFieldPK;
+import org.apache.commons.lang.builder.EqualsBuilder;
 import org.hibernate.SessionFactory;
 import org.hibernate.mapping.Column;
 import org.hibernate.mapping.Table;
@@ -27,6 +28,7 @@ import org.springframework.test.context.testng.AbstractTransactionalTestNGSpring
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
@@ -46,9 +48,9 @@ public class SynchronizerContextTest extends AbstractTransactionalTestNGSpringCo
     @Autowired
     private LocalSessionFactoryBean sessionFactoryBean;
 
-    private String[] mapColumnValues(String entityName, Map<String, String> columnValuesByName) {
+    private Serializable[] mapColumnValues(String entityName, Map<String, Serializable> columnValuesByName) {
         Table table = sessionFactoryBean.getConfiguration().getClassMapping(entityName).getTable();
-        List<String> result = new ArrayList<String>();
+        List<Serializable> result = new ArrayList<Serializable>();
         for (@SuppressWarnings("unchecked") Iterator<Column> iterator = table.getColumnIterator();
              iterator.hasNext(); ) {
             Column column = iterator.next();
@@ -56,7 +58,7 @@ public class SynchronizerContextTest extends AbstractTransactionalTestNGSpringCo
                 result.add(columnValuesByName.get(column.getName()));
             }
         }
-        return result.toArray(new String[result.size()]);
+        return result.toArray(new Serializable[result.size()]);
     }
 
     @Test
@@ -66,20 +68,21 @@ public class SynchronizerContextTest extends AbstractTransactionalTestNGSpringCo
 
         List<EvictionTarget> dummyEvictionTargets = new ArrayList<EvictionTarget>(synchronizationContext.
                 getEvictionTargets("rook.dummy_entity"));
-        String[] allFieldsFofDummy = mapColumnValues(DummyEntity.class.getName(),
-                new LinkedHashMap<String, String>() {
+        Serializable[] allFieldsFofDummy = mapColumnValues(DummyEntity.class.getName(),
+                new LinkedHashMap<String, Serializable>() {
                     {
-                        put("id", "id");
+                        put("id", 1L);
                         put("name", "name");
                     }
                 });
-        String[] keyFieldsFofDummy = mapColumnValues(DummyEntity.class.getName(),
-                new LinkedHashMap<String, String>() {
+        Serializable[] keyFieldsFofDummy = mapColumnValues(DummyEntity.class.getName(),
+                new LinkedHashMap<String, Serializable>() {
                     {
-                        put("id", "id");
+                        put("id", 1L);
                     }
                 });
-        Assert.assertEquals(dummyEvictionTargets.get(0).getPrimaryKey().of(allFieldsFofDummy), keyFieldsFofDummy);
+        Assert.assertEquals(dummyEvictionTargets.get(0).getPrimaryKey().getIdentifier(allFieldsFofDummy),
+                keyFieldsFofDummy[0]);
     }
 
     @Test
@@ -89,24 +92,21 @@ public class SynchronizerContextTest extends AbstractTransactionalTestNGSpringCo
 
         List<EvictionTarget> dummyEvictionTargets = new ArrayList<EvictionTarget>(synchronizationContext.
                 getEvictionTargets("rook.dummy_entity_2fpk"));
-        String[] allFieldsFofDummy2 = mapColumnValues(DummyEntityTwoFieldPK.class.getName(),
-                new LinkedHashMap<String, String>() {
+        Serializable[] allFieldsFofDummy2 = mapColumnValues(DummyEntityTwoFieldPK.class.getName(),
+                new LinkedHashMap<String, Serializable>() {
                     {
-                        put("id", "id");
-                        put("id2", "id2");
+                        put("id", 1L);
+                        put("id2", 2L);
                         put("name", "name");
                     }
                 });
 
-        String[] keyFieldsFofDummy2 = mapColumnValues(DummyEntityTwoFieldPK.class.getName(),
-                new LinkedHashMap<String, String>() {
-                    {
-                        put("id", "id");
-                        put("id2", "id2");
-                    }
-                });
+        DummyEntityTwoFieldPK etaKey = new DummyEntityTwoFieldPK();
+        etaKey.setId(1L);
+        etaKey.setId2(2L);
 
-        Assert.assertEquals(dummyEvictionTargets.get(0).getPrimaryKey().of(allFieldsFofDummy2), keyFieldsFofDummy2);
+        Assert.assertTrue(EqualsBuilder.reflectionEquals(
+                dummyEvictionTargets.get(0).getPrimaryKey().getIdentifier(allFieldsFofDummy2), etaKey));
     }
 
 }

rook-target-hibernate-cache/src/test/java/com/github/shyiko/rook/target/hibernate/cache/TestHelper.java
@@ -1,0 +1,50 @@
+/*
+ * Copyright 2013 Ivan Zaytsev
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.shyiko.rook.target.hibernate.cache;
+
+import org.hibernate.cfg.Configuration;
+import org.hibernate.mapping.Column;
+import org.hibernate.mapping.Table;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author <a href="mailto:ivan.zaytsev@webamg.com">Ivan Zaytsev</a>
+ *         2013-06-13
+ */
+final class TestHelper {
+    private TestHelper() {
+    }
+
+    public static Serializable[] mapColumnValues(Configuration configuration, String entityName,
+                                                 Map<String, Serializable> columnValuesByName) {
+        Table table = configuration.getClassMapping(entityName).getTable();
+        List<Serializable> result = new ArrayList<Serializable>();
+        for (@SuppressWarnings("unchecked") Iterator<Column> iterator = table.getColumnIterator();
+             iterator.hasNext(); ) {
+            Column column = iterator.next();
+            if (columnValuesByName.containsKey(column.getName())) {
+                result.add(columnValuesByName.get(column.getName()));
+            }
+        }
+        return result.toArray(new Serializable[result.size()]);
+    }
+}

rook-target-hibernate-cache/src/test/java/com/github/shyiko/rook/target/hibernate/cache/model/DummyEntityTwoFieldPK.java
@@ -15,10 +15,11 @@
  */
 package com.github.shyiko.rook.target.hibernate.cache.model;
 
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import java.io.Serializable;
@@ -27,11 +28,12 @@ import java.io.Serializable;
  * @author <a href="mailto:ivan.zaytsev@webamg.com">Ivan Zaytsev</a>
  */
 @Entity
+@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE, region = "CORE_REGION")
 @Table(name = "dummy_entity_2fpk")
 public class DummyEntityTwoFieldPK implements Serializable {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    //@GeneratedValue(strategy = GenerationType.AUTO)
     @Column
     private Long id;
 
@@ -39,7 +41,7 @@ public class DummyEntityTwoFieldPK implements Serializable {
     private String name;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    //@GeneratedValue(strategy = GenerationType.AUTO)
     @Column
     private Long id2;
 

commit 783209daab1445bec528681f7f45c3fcf0b3789d

Fixed failing integration tests caused by changes introduced in 8134ecf5b7058e03c56a0dc2f1857617e6e0ee09

rook-target-hibernate-cache/src/main/java/com/github/shyiko/rook/target/hibernate/cache/PrimaryKey.java
@@ -48,7 +48,7 @@ public class PrimaryKey {
     }
 
     public PrimaryKey(Collection collection) {
-        this(collection.getKey(), collection.getTable());
+        this(collection.getKey(), collection.getCollectionTable());
     }
 
     public PrimaryKey(PersistentClass persistentClass) {